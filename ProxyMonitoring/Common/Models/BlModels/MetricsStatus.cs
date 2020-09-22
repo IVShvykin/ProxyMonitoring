80761631bc62a26a61414999fd0b03bc04d38c7a
Швыкин Семен <Sshvykin@innotehnum.com>
2020-09-22 13:26:29 +0300

    Моделька метрики

---
+++ ProxyMonitoring/Common/Models/BlModels/MetricsStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Models.BlModels
+{
+    public enum MetricsStatus
+    {
+        normal = 0,
+        warning
+    }
+}
---
+++ ProxyMonitoring/Common/Models/VmModels/MetricsResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using Common.Models.BlModels;
+
+namespace Common.Models.VmModels
+{
+    public class MetricsResModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool Mine { get; set; }
+        public string Value { get; set; }
+        public long Delta { get; set; }
+        public double DeltaPercent { get; set; }
+        public MetricsStatus DeltaStatus { get; set; }
+        public double TotalPercent { get; set; }
+    }
+}
