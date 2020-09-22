2751ab0b0d8d6fdb88e72a25048e75c710474685
Швыкин Семен <Sshvykin@innotehnum.com>
2020-09-22 14:21:30 +0300

    SystemModel

---
+++ ProxyMonitoring/Common/Models/VmModels/SystemResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.Models.VmModels
+{
+    public class SystemResponseModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool Mine { get; set; } = false;
+        public int CriticalAccidents { get; set; } = 0;
+        public int MajorAccedents { get; set; } = 0;
+        public int MinorAccedents { get; set; } = 0;
+    }
+}
