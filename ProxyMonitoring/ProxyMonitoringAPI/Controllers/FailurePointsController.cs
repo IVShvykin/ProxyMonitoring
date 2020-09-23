2a6738b40f972fcec27596d9ebea1811ba406f67
IVShvykin <IVShvykin@gmail.com>
2020-09-23 13:45:13 +0300

    failure

--- ProxyMonitoring/ProxyMonitoringAPI/Controllers/FailurePointsController.cs
+++ ProxyMonitoring/ProxyMonitoringAPI/Controllers/FailurePointsController.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using vmMock = Common.Mock.VmMock.VmMock;
 
 namespace ProxyMobileMonitoringAPI.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [ApiVersion("1.0")]
+    [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+    [Route("api/v{version:apiVersion}/[controller]")]
     public class FailurePointsController : ControllerBase
     {
-       
+        [HttpGet]
+        public async Task<string[]> Get()
+        {
+            return vmMock.failurePoints;
+        }
     }
 }
