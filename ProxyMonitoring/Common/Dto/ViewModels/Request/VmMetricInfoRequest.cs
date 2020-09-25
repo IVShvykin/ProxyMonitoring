b8867cadcd1bb0f1e59e4ce51bb2e5165f9a8868
IVShvykin <IVShvykin@gmail.com>
2020-09-25 16:45:54 +0300

    fix

--- ProxyMonitoring/Common/Dto/ViewModels/Request/VmMetricInfoRequest.cs
+++ ProxyMonitoring/Common/Dto/ViewModels/Request/VmMetricInfoRequest.cs
@@ -2,10 +2,11 @@
 
 namespace Common.Dto.ViewModels.Request
 {
-    public class VmMetricInfoRequest
+    public class VmMetricInfoRequest : VmPageRequestBase
     {
         public string Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime FinishDate { get; set; }
+        public string Keyword { get; set; }
     }
 }
